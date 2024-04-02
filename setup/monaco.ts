c089611048e327db658afafc3ef20f69e7cd0cc4
Daniel Raniz Raneland <raniz@factor10.com>
2024-04-02 22:29:28 +0200

    Update theme for latest SliDev

--- setup/monaco.ts
+++ setup/monaco.ts
@@ -1,17 +1,5 @@
 import { defineMonacoSetup } from '@slidev/types'
 
-export default defineMonacoSetup(async(monaco) => {
-  await Promise.all([
-    // load theme
-    (async() => {
-      const { default: dark } = await import('theme-vitesse/themes/vitesse-dark.json')
-      const { default: light } = await import('theme-vitesse/themes/vitesse-light.json')
-
-      light.colors['editor.background'] = '#00000000'
-      dark.colors['editor.background'] = '#00000000'
-
-      monaco.editor.defineTheme('vitesse-light', light as any)
-      monaco.editor.defineTheme('vitesse-dark', dark as any)
-    })(),
-  ])
+export default defineMonacoSetup((monaco) => {
+  monaco.editor.setTheme('solarized-light');
 })
