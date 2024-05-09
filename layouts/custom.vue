af405832d7abebc62aaae8949f67837214d3a99f
Daniel Raniz Raneland <raniz@factor10.com>
2024-05-09 17:28:26 +0100

    Custom layout

---
+++ layouts/custom.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="full w-full h-full">
+    <slot />
+    <factor10-logo></factor10-logo>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from 'vue'
+import Default from '../layouts/default.vue'
+import Factor10Logo from "../components/Factor10Logo.vue";
+
+const components = defineComponent({ Default })
+</script>
