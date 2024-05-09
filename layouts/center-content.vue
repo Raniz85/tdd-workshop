1ff5e711e0e67520ed8b248904e9b13a3ff6c767
Daniel Raniz Raneland <raniz@factor10.com>
2024-05-09 17:32:21 +0100

    Add center content layout

---
+++ layouts/center-content.vue
@@ -0,0 +1,17 @@
+<template>
+  <default class="full w-full h-full max-w-full max-h-full flex flex-col">
+    <slot />
+    <div class="grow flex-basis-none table">
+      <div class="table-cell vertical-middle">
+        <slot name="content"/>
+      </div>
+    </div>
+  </default>
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from 'vue'
+import Default from '../layouts/default.vue'
+
+const components = defineComponent({ Default })
+</script>
